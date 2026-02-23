f06c009e55ed691630f2a05d45d89176225f4613
mansi-4124 <shintrems4@gmail.com>
2026-02-23 09:38:37 +0530

    Backend: Setup with Prisma and Swagger

---
+++ backend/src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function setupSwagger(app: INestApplication) {
+
+  const config = new DocumentBuilder()
+    .setTitle('Survix API')
+    .setDescription('Backend API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, doc);
+}
